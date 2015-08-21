c474d94ed246e3d0dd3afa6bdcb504879a102dc4
Kishor Bhat <kishorbhat@gmail.com>
2015-08-21 15:44:34 +0530

    add RSA explanation

--- server.py
+++ server.py
@@ -103,6 +103,20 @@ class Base64(Cipher):
         self.socket.recv(2048)
         self.cipherGreeting()
 
+class RSA(Cipher):
+
+    def explain(self):
+            self.socket.send("The RSA cryptosystem is based on asymmetric key cryptography.\nThis means that the keys used for encryption and decryption are different.\n")
+            self.socket.send("We have three main stages:\n(a) Encryption\n(b) Decryption\n(c) Key Generation\n\n")
+            self.socket.send("(a) Encryption\ny = (x ** e) mod n\nHere, x is the binary value of the plaintext, y is the ciphertext.\nThe pair (n, e) is referred to as the public key, and 'e' is the public exponent or encrypting exponent.\n\n")
+            self.socket.send("(b) Decryption\nx = (y ** d) mod n\nHere, x, y and n are the same, and d is the private exponent/key or decrypting exponent.\n\n")
+            self.socket.send("CONSTRAINTS:\n1. It must be computationally infeasible to obtain the private key from the public key (n, e)\n2. Encryption and decryption should be easy given the parameters. Fast exponentiation is necessary.\n3. We cannot encrypt more than L bits of plaintext, where L is the bit size of n.\n")
+            self.socket.send("4. Given n, there should be many possible values for e and d. Otherwise, we can brute force the private key.\n\n")
+            self.socket.send("(c) Key Generation\nThis is how n, e and d are obtained.\n1. Choose two prime numbers, p and q.\n2. n = p * q\n3. Compute a quantity phi (henceforth P) as P = (p - 1) * (q - 1)")
+            self.socket.send("4. Choose 'e' such that 0 < e < P and GCD(e, P) is 1.\nMathematically speaking, e and P are relatively prime.\n")
+            self.socket.send("5. Compute private key d as (d * e) is congruent to 1 mod P.\nOn rearranging, d = t mod P, where t is the inverse of e.\n")
+            
+
 class ClientThread(threading.Thread):
 
     def __init__(self,ip,port,socket):
@@ -127,6 +141,7 @@ class ClientThread(threading.Thread):
             self.socket.send(row_format.format("3", "XOR Cipher") + "\n\n")
             self.socket.send(row_format.format("4", "Dvorak Cipher") + "\n\n")
             self.socket.send(row_format.format("5", "Base64 Cipher") + "\n\n") # lol
+            self.socket.send(row_format.format("6", "RSA Cryptosystem") + "\n\n")
             self.socket.send("Enter choice: ")
             data = self.socket.recv(2048).strip()
             print "Client sent : " + data
@@ -144,6 +159,9 @@ class ClientThread(threading.Thread):
                 ins.cipherGreeting()
             elif (data == '5'):
                 ins = Base64(self.socket)
+                ins.cipherGreeting()
+            elif (data == '6'):
+                ins = RSA(self.socket)
                 ins.cipherGreeting()
 
         print "Client disconnected..."
