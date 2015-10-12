32028eecdb096f00aa4ab41d437b323d57575a24
Kishor Bhat <kishorbhat@gmail.com>
2015-10-12 21:18:59 +0530

    Refactoring and documentation

--- src/server.py
+++ src/server.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 import socket, threading
 import string
-import b64, bacon, diffie, dvorak, md5, rsa, shift, vigenere, xor
+import cipher
+import ast
 
-
+# Set the output width for formatted strings
 row_format ="{:>30}" * 2
+
+# Maps input option to a cipher class string
+cipherClasses = {
+    '1': 'ShiftCipher',
+    '2': 'BaconCipher',
+    '3': 'XORCipher',
+    '4': 'DvorakCipher',
+    '5': 'Base64',
+    '6': 'RSA',
+    '7': 'VigenereCipher',
+    '8': 'MD5',
+    '9': 'DiffieHelman'
+}
             
 class ClientThread(threading.Thread):
 
@@ -15,14 +29,16 @@ class ClientThread(threading.Thread):
         self.socket = socket
         print "[+] New thread started for "+ip+":"+str(port)
 
-    def run(self):    
+    def run(self):
         print "Connection from : "+ip+":"+str(port)
         data = "dummydata"
 
-        while len(data):
-            mainMenuPrompt = 'Welcome!\nHere you can learn about different encryption schemes\nand use them to encrypt your plaintext! Have fun!\nEnter the appropriate number to continue.\n\n'
+        mainMenuPrompt = 'Welcome!\nHere you can learn about different encryption schemes\nand use them to encrypt your plaintext! Have fun!\n'
+        self.socket.send(mainMenuPrompt)
 
-            self.socket.send(mainMenuPrompt)
+        while len(data):
+            # Present main menu
+            self.socket.send('Enter the appropriate number to continue.\n\n')
             self.socket.send(row_format.format("Serial Number", "Encryption Scheme") + "\n")
             self.socket.send(row_format.format("-------------", "-----------------") + "\n")
             self.socket.send(row_format.format("1", "Shift Cipher") + "\n\n")
@@ -35,37 +51,25 @@ class ClientThread(threading.Thread):
             self.socket.send(row_format.format("8", "MD5") + "\n\n")
             self.socket.send(row_format.format("9", "Diffie Helman Key Exchange") + "\n\n")
             self.socket.send("Enter choice ('^C' to exit): ")
+
+            # Accept user input
             data = self.socket.recv(2048).strip()
             print "Client sent : " + data
-            if (data == '1'):
-                ins = shift.ShiftCipher(self.socket)
-                ins.cipherGreeting()
-            elif (data == '2'):
-                ins = bacon.BaconCipher(self.socket)
-                ins.cipherGreeting()
-            elif (data == '3'):
-                ins = xor.XORCipher(self.socket)
-                ins.cipherGreeting()
-            elif (data == '4'):
-                ins = dvorak.DvorakCipher(self.socket)
-                ins.cipherGreeting()
-            elif (data == '5'):
-                ins = b64.Base64(self.socket)
-                ins.cipherGreeting()
-            elif (data == '6'):
-                ins = rsa.RSA(self.socket)
-                ins.cipherGreeting()
-            elif (data == '7'):
-                ins = vigenere.VigenereCipher(self.socket)
-                ins.cipherGreeting()
-            elif (data == '8'):
-                ins = md5.MD5(self.socket)
-                ins.cipherGreeting()
-            elif (data == '9'):
-                ins = diffie.DiffieHelman(self.socket)
-                ins.cipherGreeting()
-            elif (data == 'q'):
+
+            # Termination!
+            if (data == 'q'):
                 exit()
+
+            try:
+                # getattr lets us map a string to the corresponding classname
+                ins = getattr(cipher, cipherClasses[data])(self.socket)
+            except:
+                # When they've entered anything other than [1-9]...
+                self.socket.send("Invalid option!\n")
+                continue
+            # Display cipher submenu
+            ins.cipherGreeting()
+
         print "Client disconnected..."
 
 host = "0.0.0.0"
@@ -81,6 +85,8 @@ while True:
     tcpsock.listen(4)
     print "\nListening for incoming connections..."
     (clientsock, (ip, port)) = tcpsock.accept()
+
+    # Let's use a new thread for each incoming connection
     newthread = ClientThread(ip, port, clientsock)
     newthread.start()
     threads.append(newthread)
